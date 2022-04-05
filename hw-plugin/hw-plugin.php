8d567d48eed3abe676e12261a6e2e042619e0a73
Jesse <70843294+jessep92@users.noreply.github.com>
2022-04-04 23:43:22 -0700

    Add menu function/action hook

--- hw-plugin/hw-plugin.php
+++ hw-plugin/hw-plugin.php
@@ -17,3 +17,21 @@
  * Text Domain: hello-world
  */
 ?>
+
+<?php
+function print_hello_world_title() {
+    echo "<h1>Hello World!</h1>";
+}
+
+function hello_world_admin_menu() {
+    add_menu_page(
+        'Hello World!', // Page Title
+        'Hello World Menu Title', // Menu Title
+        'manage_options', // Capability
+        'hello-world', // Menu Slug
+        'print_hello_world_title' // Callback Function
+    );
+}
+
+add_action('admin_menu', 'hello_world_admin_menu');
+?>
