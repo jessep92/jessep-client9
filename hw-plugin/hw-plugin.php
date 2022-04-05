43b8892ba171451b74f8ca7a671f1bc911b1de12
Jesse <70843294+jessep92@users.noreply.github.com>
2022-04-04 23:36:12 -0700

    Add plugin header

---
+++ hw-plugin/hw-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 'Hello World!' Plugin
+ * 
+ * @package HelloWorld
+ * @author Jesse Peltonen
+ * @copyright 2022 Jesse Peltonen
+ * @license GPL-2.0-or-later
+ * 
+ * @wordpress-plugin
+ * Plugin Name: 'Hello World!'
+ * Plugin URI: http://client9.jpeltonen.imgd.ca/hello-world
+ * Description: Prints 'Hello World!' in WordPress Admin
+ * Vers. 0.0.1
+ * Author: Jesse Peltonen
+ * Author URI: http://client9.jpeltonen.imgd.ca
+ * Text Domain: hello-world
+ */
+?>
